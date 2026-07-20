7bcd7398a2b4c3845f9b8861009904290193978f
Y7tion <farnitin@yandex.ru>
2026-07-21 01:00:13 +0400

    add proxy settings

--- core/src/main/java/ru/javabot/TelegramBotApplication.java
+++ core/src/main/java/ru/javabot/TelegramBotApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TelegramBotApplication {
 	public static void main(String[] args) {
+		System.setProperty("http.proxyHost", "157.20.121.43");
+		System.setProperty("http.proxyPort", "1080");
+		System.setProperty("https.proxyHost", "157.20.121.43");
+		System.setProperty("https.proxyPort", "1080");
+
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
 }
