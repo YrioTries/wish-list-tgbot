c6b93876581b1da4362c0a8f232ba9475ea81bc6
Y7tion <farnitin@yandex.ru>
2026-07-21 03:48:26 +0400

    add simple function

--- core/src/main/java/ru/javabot/UpdateConsumer.java
+++ core/src/main/java/ru/javabot/UpdateConsumer.java
@@ -7,10 +7,14 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
@@ -29,59 +33,105 @@ public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
         );
     }
 
-//    public UpdateConsumer() {
-//        this.telegramClient = new OkHttpTelegramClient(
-//                "8700103330:AAEUfG404m-8yizqEvsv5eMlsZ3R3f3YRIU"
-//        );
-//    }
-
-
-
     @SneakyThrows
     @Override
     public void consume(Update update) {
         if (update.hasMessage()) {
             String messageText = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
+
             if (messageText.equals("/start")) {
-                sandMainMenu(chatId);
-            } else {
+                sendMainMenu(chatId);
+            } else if (messageText.equals("/keyboard")) {
+                sendKeyBoard(chatId);
+            } else if (messageText.equals("Привет")) {
+                sendMyName(chatId, update.getMessage().getFrom());
+            }
+            else {
                 System.out.printf(
                         "Пришло сообщение: %s от %s",
                         messageText,
                         chatId
                 );
 
-                SendMessage message = SendMessage.builder()
-                        .text("Я не понимаю тебя((")
-                        .chatId(chatId)
-                        .build();
+                sendMessage(chatId, "Я не понимаю тебя(("
+                );
 
-                telegramClient.execute(message);
             }
+        } else if (update.hasCallbackQuery()) {
+            handleCallBackQuery(update.getCallbackQuery());
+
         }
-
-
     }
 
-    private void sandMainMenu(Long chatId) throws TelegramApiException {
+    private void sendMyName(Long chatId, User user) {
+        var text = "Привет!\n\nТебя зовут: %s\nВаш ник: @%s"
+                .formatted(
+                        user.getFirstName() + " " + user.getLastName(),
+                        user.getUserName()
+                );
+
+        sendMessage(chatId, text);
+    }
+
+    private void sendKeyBoard(Long chatId) throws TelegramApiException {
+
+        SendMessage message = SendMessage.builder()
+                .text("Обычная клавиатура")
+                .chatId(chatId)
+                .build();
+
+        List<KeyboardRow> keyboardRows = List.of(
+                new KeyboardRow("Привет", "Пока", "Ариведерчи")
+        );
+
+        ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup(keyboardRows);
+        message.setReplyMarkup(markup);
+        telegramClient.execute(message);
+    }
+
+
+    private void handleCallBackQuery(CallbackQuery callbackQuery) {
+        var data = callbackQuery.getData();
+        var chatId = callbackQuery.getFrom().getId();
+        var user = callbackQuery.getFrom();
+
+        switch (data) {
+            case "first_button" -> sendMessage(chatId, "Создаю новый список");
+            case "second_button" -> sendMessage(chatId, "Добавляю желание");
+            case "third_button" -> sendMessage(chatId, "Удаляю список");
+            default -> sendMessage(chatId, "Нажмите на кнопку");
+        }
+    }
+
+    @SneakyThrows
+    private void sendMessage(Long chatId, String messageText) {
+        SendMessage message = SendMessage.builder()
+                .text(messageText)
+                .chatId(chatId)
+                .build();
+
+        telegramClient.execute(message);
+    }
+
+    private void sendMainMenu(Long chatId) throws TelegramApiException {
         SendMessage message = SendMessage.builder()
                 .text("Выберете действие:")
                 .chatId(chatId)
                 .build();
         var button1 = InlineKeyboardButton.builder()
                 .text("Создать новый список")
-                .callbackData("my_name")
+                .callbackData("first_button")
                 .build();
 
         var button2 = InlineKeyboardButton.builder()
                 .text("Добавить желание")
-                .callbackData("your_name")
+                .callbackData("second_button")
                 .build();
 
         var button3 = InlineKeyboardButton.builder()
                 .text("Удалить список")
-                .callbackData("their_name")
+                .callbackData("third_button")
                 .build();
         List<InlineKeyboardRow> keyboardRows = List.of(
                 new InlineKeyboardRow(button1),
