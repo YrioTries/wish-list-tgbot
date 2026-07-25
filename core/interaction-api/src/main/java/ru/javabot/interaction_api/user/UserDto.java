d278254a51596a3fa594162e79ed0e3730df9f68
Y7tion <farnitin@yandex.ru>
2026-07-25 04:46:12 +0400

    add UserDto

---
+++ core/interaction-api/src/main/java/ru/javabot/interaction_api/user/UserDto.java
@@ -0,0 +1,15 @@
+package ru.javabot.interaction_api.user;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserDto {
+    Long id;
+    String nickname;
+}
