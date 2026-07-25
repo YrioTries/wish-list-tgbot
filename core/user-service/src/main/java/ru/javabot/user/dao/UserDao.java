85ba0af71596749684c7cc4fc3ad8d76a452581a
Y7tion <farnitin@yandex.ru>
2026-07-25 04:45:35 +0400

    add UserDao fields

--- core/user-service/src/main/java/ru/javabot/user/dao/UserDao.java
+++ core/user-service/src/main/java/ru/javabot/user/dao/UserDao.java
@@ -1,4 +1,20 @@
 package ru.javabot.user.dao;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = @UniqueConstraint(columnNames = "telegram_user_id")
+)
 public class UserDao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "telegram_user_id", nullable = false, unique = true)
+    private Long userChatId;
+
+    @Column(name = "nickname", length = 50)
+    private String nickname;
 }
