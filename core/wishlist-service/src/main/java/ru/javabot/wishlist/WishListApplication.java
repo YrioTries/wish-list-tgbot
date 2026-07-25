c00f71e0e29ab4f8b5e3b72491255d1d9cdc3650
Y7tion <farnitin@yandex.ru>
2026-07-25 04:24:03 +0400

    add WishList-service module

---
+++ core/wishlist-service/src/main/java/ru/javabot/wishlist/WishListApplication.java
@@ -0,0 +1,11 @@
+package ru.javabot.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WishListApplication.class, args);
+    }
+}
