6d16d58c744b367b42c919e2df66f7ebaa66b6a0
Y7tion <farnitin@yandex.ru>
2026-07-25 04:23:11 +0400

    add WishDto

---
+++ core/interaction-api/src/main/java/ru/javabot/interaction_api/wish/WishDto.java
@@ -0,0 +1,19 @@
+package ru.javabot.interaction_api.wish;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class WishDto {
+    Long id;
+    @NotBlank
+    @Size(min = 3, max = 50)
+    String name;
+    String description;
+}
