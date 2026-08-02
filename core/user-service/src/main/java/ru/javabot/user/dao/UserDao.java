cbf50dcf7f8f3d2e81e5e11476848548c365ace5
Y7tion <farnitin@yandex.ru>
2026-08-02 05:35:51 +0400

    rename fields and add some annotations

--- core/user-service/src/main/java/ru/javabot/user/dao/UserDao.java
+++ core/user-service/src/main/java/ru/javabot/user/dao/UserDao.java
@@ -1,20 +1,28 @@
 package ru.javabot.user.dao;
 
 import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @Entity
 @Table(
         name = "users",
-        uniqueConstraints = @UniqueConstraint(columnNames = "telegram_user_id")
+        uniqueConstraints = @UniqueConstraint(columnNames = "telegram_chat_id")
 )
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
 
-    @Column(name = "telegram_user_id", nullable = false, unique = true)
-    private Long userChatId;
+    @Column(name = "telegram_chat_id", nullable = false, unique = true)
+    Long userChatId;
 
     @Column(name = "nickname", length = 50)
-    private String nickname;
+    String nickname;
 }
