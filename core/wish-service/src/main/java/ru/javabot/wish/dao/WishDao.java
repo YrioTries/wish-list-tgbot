23fc5d66ca807d3063ec95ef6c7fc4b8da2c4147
Y7tion <farnitin@yandex.ru>
2026-08-02 05:36:34 +0400

    add field ownerId in WishDao

--- core/wish-service/src/main/java/ru/javabot/wish/dao/WishDao.java
+++ core/wish-service/src/main/java/ru/javabot/wish/dao/WishDao.java
@@ -3,6 +3,7 @@ package ru.javabot.wish.dao;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import ru.javabot.interaction_api.user.dto.UserDto;
 
 @Entity
 @Table(name = "wishes")
@@ -20,6 +21,9 @@ public class WishDao {
 
     @Column(name = "wish_name")
     String name;
+
+    @Column(name = "owner_id", nullable = false)
+    Long ownerId;
 
     @Column(name = "wish_description")
     String description;
