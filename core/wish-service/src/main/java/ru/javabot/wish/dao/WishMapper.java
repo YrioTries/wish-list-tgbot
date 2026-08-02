a9335a927aab553614f6873144c8f1d96fe475ee
Y7tion <farnitin@yandex.ru>
2026-08-02 06:22:51 +0400

    add wish mapper

---
+++ core/wish-service/src/main/java/ru/javabot/wish/dao/WishMapper.java
@@ -0,0 +1,14 @@
+package ru.javabot.wish.dao;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import ru.javabot.interaction_api.wish.WishDto;
+
+@Mapper(componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface WishMapper {
+
+    WishDto toDto(WishDao UserDao);
+
+    WishDao toDao(WishDto WishDto);
+}
