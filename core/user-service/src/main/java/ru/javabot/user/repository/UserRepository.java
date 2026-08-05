30c8314a2493c91518a9bde05f164340c8e8db10
Y7tion <farnitin@yandex.ru>
2026-08-06 03:30:47 +0400

    add findByNickname and existsByNickname

--- core/user-service/src/main/java/ru/javabot/user/repository/UserRepository.java
+++ core/user-service/src/main/java/ru/javabot/user/repository/UserRepository.java
@@ -8,4 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserDao, Long> {
+
+    Optional<UserDao> findByNickname(String nickname);
+
+    boolean existsByNickname(String nickname);
 }
