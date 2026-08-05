a2503f0c08a52e073a278c906b33cc459fbe5fc0
Y7tion <farnitin@yandex.ru>
2026-08-06 03:31:30 +0400

    add UserServices and their methods

---
+++ core/user-service/src/main/java/ru/javabot/user/service/UserService.java
@@ -0,0 +1,4 @@
+package ru.javabot.user.service;
+
+public interface UserService {
+}
---
+++ core/user-service/src/main/java/ru/javabot/user/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package ru.javabot.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javabot.interaction_api.exeption.NotFoundException;
+import ru.javabot.interaction_api.user.dto.UserDto;
+import ru.javabot.user.dao.UserDao;
+import ru.javabot.user.dao.UserMapper;
+import ru.javabot.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceImpl {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserDto findUserByUsername(String nickname) {
+        UserDao user = userRepository.findByNickname(nickname)
+                .orElseThrow(() -> new NotFoundException("Пользователь с никнеймом " + nickname + " не найден"));
+
+        return userMapper.toDto(user);
+    }
+
+    private void checkUserNickname(String nickname) {
+        if (!userRepository.existsByNickname(nickname)) {
+            throw new NotFoundException("No user with the same nickname");
+        }
+    }
+}
