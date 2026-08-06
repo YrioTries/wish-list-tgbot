cb0aa077fbb91993ca43a9e718aaa8b8d43b70f6
Y7tion <farnitin@yandex.ru>
2026-08-06 04:05:51 +0400

    add UserService interface methods addNewUser and updateNickname

--- core/user-service/src/main/java/ru/javabot/user/service/UserService.java
+++ core/user-service/src/main/java/ru/javabot/user/service/UserService.java
@@ -4,4 +4,8 @@ import ru.javabot.interaction_api.user.dto.UserDto;
 
 public interface UserService {
     UserDto findUserByUsername(String nickname);
+
+    UserDto addNewUser(UserDto userDto);
+
+    UserDto updateNickname(Long chatId, String newNickname);
 }
