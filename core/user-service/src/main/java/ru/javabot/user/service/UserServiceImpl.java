eb15250d69d5d277ac403d2cd8a7e55b52689e9a
Y7tion <farnitin@yandex.ru>
2026-08-07 02:03:24 +0400

    add @Override annotation in UserServiceImpl

--- core/user-service/src/main/java/ru/javabot/user/service/UserServiceImpl.java
+++ core/user-service/src/main/java/ru/javabot/user/service/UserServiceImpl.java
@@ -17,6 +17,7 @@ public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
+    @Override
     public UserDto findUserByUsername(String nickname) {
         UserDao user = userRepository.findByNickname(nickname)
                 .orElseThrow(() -> new NotFoundException("Пользователь с никнеймом " + nickname + " не найден"));
@@ -24,6 +25,7 @@ public class UserServiceImpl implements UserService{
         return userMapper.toDto(user);
     }
 
+    @Override
     @Transactional
     public UserDto addNewUser(UserDto userDto) {
         if (userRepository.existsByNickname(userDto.getNickname())) {
@@ -34,6 +36,7 @@ public class UserServiceImpl implements UserService{
         return userMapper.toDto(userRepository.save(userDao));
     }
 
+    @Override
     @Transactional
     public UserDto updateNickname(Long chatId, String newNickname) {
         UserDao userDao = userRepository.findByTelegramChatId(chatId)
