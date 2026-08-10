c7d6ffc260db4bab2af6288f2807af70f8874cef
Y7tion <farnitin@yandex.ru>
2026-08-11 02:48:18 +0400

    rename from CreateWishRequest to CreateWishlistRequest

--- core/interaction-api/src/main/java/ru/javabot/interaction_api/wishlist/dto/CreateWishlistRequest.java
+++ core/interaction-api/src/main/java/ru/javabot/interaction_api/wishlist/dto/CreateWishlistRequest.java
@@ -11,7 +11,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class CreateWishRequest {
+public class CreateWishlistRequest {
 
     @NotBlank
     @Size(min = 3, max = 150)
