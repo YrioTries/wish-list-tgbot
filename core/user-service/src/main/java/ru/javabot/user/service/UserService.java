069e37da83332b88c6d21224fe8825695142b3b4
Y7tion <farnitin@yandex.ru>
2026-08-11 02:49:46 +0400

    change type dto in UserService

--- core/user-service/src/main/java/ru/javabot/user/service/UserService.java
+++ core/user-service/src/main/java/ru/javabot/user/service/UserService.java
@@ -1,11 +1,12 @@
 package ru.javabot.user.service;
 
+import ru.javabot.interaction_api.user.dto.CreateUserRequest;
 import ru.javabot.interaction_api.user.dto.UserDto;
 
 public interface UserService {
     UserDto findUserByUsername(String nickname);
 
-    UserDto addNewUser(UserDto userDto);
+    UserDto addNewUser(CreateUserRequest userRequest);
 
     UserDto updateNickname(Long chatId, String newNickname);
 }
