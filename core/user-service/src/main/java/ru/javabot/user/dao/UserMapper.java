7cbb769c84759306a8671c50c72526501f84b41c
Y7tion <farnitin@yandex.ru>
2026-08-11 02:49:26 +0400

    change type dto in UserMapper

--- core/user-service/src/main/java/ru/javabot/user/dao/UserMapper.java
+++ core/user-service/src/main/java/ru/javabot/user/dao/UserMapper.java
@@ -2,13 +2,14 @@ package ru.javabot.user.dao;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import ru.javabot.interaction_api.user.dto.CreateUserRequest;
 import ru.javabot.interaction_api.user.dto.UserDto;
 
 @Mapper(componentModel = "spring",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface UserMapper {
 
-    UserDto toDto(UserDao UserDao);
+    UserDto toDto(UserDao userDao);
 
-    UserDao toDao(UserDto UserDto);
+    UserDao toDao(CreateUserRequest userRequest);
 }
