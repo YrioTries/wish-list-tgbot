320812fb4ae15f88e0d5d76e188568d13cb34237
Y7tion <farnitin@yandex.ru>
2026-08-14 03:00:20 +0400

    add WishlistService and WishlistServiceImpl.java

--- core/wishlist-service/src/main/java/ru/javabot/wishlist/service/WishlistServiceImpl.java
+++ core/wishlist-service/src/main/java/ru/javabot/wishlist/service/WishlistServiceImpl.java
@@ -3,17 +3,24 @@ package ru.javabot.wishlist.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javabot.interaction_api.wishlist.dto.CreateWishlistRequest;
 import ru.javabot.interaction_api.wishlist.dto.WishlistDto;
+import ru.javabot.wishlist.dao.WishlistDao;
+import ru.javabot.wishlist.dao.WishlistMapper;
 import ru.javabot.wishlist.repository.WishlistRepository;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class WishlistServiceImpl {
+public class WishlistServiceImpl implements WishlistService{
+    private final WishlistMapper wishlistMapper;
     private final WishlistRepository wishlistRepository;
 
     @Transactional
-    public WishlistDto addNewWish() {
-        return new WishlistDto();
+    public WishlistDto addNewWish(Long ownerId, CreateWishlistRequest wishlistRequest) {
+        WishlistDao wishlistDao = wishlistMapper.toDao(wishlistRequest);
+        wishlistDao.setOwnerId(ownerId);
+        WishlistDao wishlistDaoUpdated = wishlistRepository.save(wishlistDao);
+        return wishlistMapper.toDto(wishlistDaoUpdated);
     }
 }
