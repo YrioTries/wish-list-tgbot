5532501b3efd00642312a88fc90ccdbd2ec7a43a
Y7tion <farnitin@yandex.ru>
2026-08-14 02:53:54 +0400

    change fields in CreateWishlistRequest

--- core/interaction-api/src/main/java/ru/javabot/interaction_api/wishlist/dto/CreateWishlistRequest.java
+++ core/interaction-api/src/main/java/ru/javabot/interaction_api/wishlist/dto/CreateWishlistRequest.java
@@ -1,7 +1,6 @@
 package ru.javabot.interaction_api.wishlist.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -17,11 +16,6 @@ public class CreateWishlistRequest {
     @Size(min = 3, max = 150)
     String name;
 
-    String reference;
-
-    @PositiveOrZero
-    Long expectedPrice;
-
-    @Size(max = 1000)
-    String description;
+    @NotBlank
+    AccessRights accessRights;
 }
